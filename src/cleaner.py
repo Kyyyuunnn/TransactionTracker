87403a775291fadd2adeda443d196ef8b9019450
Steven Nguyen <stevennguyen4200@gmail.com>
2025-05-22 16:47:54 -0500

    added removing rows with blanks

--- src/cleaner.py
+++ src/cleaner.py
@@ -3,10 +3,15 @@ import pandas as pd
 
 df = loadExcel()
 
-#vendorList = [] could track later on
-amtList = []
-categoryList = []
-dateList = []
+# #vendorList = [] could track later on
+# amtList = []
+# categoryList = []
+# dateList = []
+
+# removing rows with blanks
+df = df.dropna(subset = ["Vendor", "Category", "Amount", "Date"])
+
+# make sure each category is the correct format/type
 
 # grabbing the amounts and round total (by 2 cents)
 amounts = df['Amount']
@@ -15,7 +20,9 @@ amtList = amounts.tolist()
 total = sum(amtList)
 roundedTotal = round(total, 2)
 
-print(roundedTotal)
+# Category totals
+categoryTotals = df.groupby("Category")["Amount"].sum().round(2)
+
 
 
 
