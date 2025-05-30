f9d257eb70924665e672add8cd8899f2bb413c8b
Steven Nguyen <stevennguyen4200@gmail.com>
2025-05-30 13:02:34 -0500

    adds all amounts even if other category blank but not vice versa

--- src/cleaner.py
+++ src/cleaner.py
@@ -9,20 +9,27 @@ df = loadExcel()
 # dateList = []
 
 # removing rows with blanks
-df = df.dropna(subset = ["Vendor", "Category", "Amount", "Date"])
+if df is not None:
+    df = df[~(
+        df["Amount"].isnull() & 
+        df[["Vendor", "Category", "Date"]].isnull().all(axis=1)
+    )]
+
 
 # make sure each category is the correct format/type
+df["Amount"] = pd.to_numeric(df["Amount"], errors = "coerce")
+
 
 # grabbing the amounts and round total (by 2 cents)
 amounts = df['Amount']
 pd.to_numeric(amounts)
-amtList = amounts.tolist()
+amtList = amounts.dropna().tolist()
 total = sum(amtList)
 roundedTotal = round(total, 2)
 
 # Category totals
-categoryTotals = df.groupby("Category")["Amount"].sum().round(2)
+categoryTotals = df.dropna(subset=["Category", "Amount"]).groupby("Category")["Amount"].sum().round(2)
 
-
+print(categoryTotals)
 
 
