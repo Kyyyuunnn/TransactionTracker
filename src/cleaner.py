3dd2b2e440849d736383f258b3317d1d9be39338
Steven Nguyen <stevennguyen4200@gmail.com>
2025-06-03 13:11:45 -0500

    checking categories for issues

--- src/cleaner.py
+++ src/cleaner.py
@@ -1,5 +1,6 @@
 from config import loadExcel
 import pandas as pd
+import matplot.lib.pyplot as plt
 
 df = loadExcel()
 
@@ -14,6 +15,8 @@ if df is not None:
 # checking columns
 df["Amount"] = pd.to_numeric(df["Amount"], errors = "coerce")
 df["Category"] = df["Category"].apply(lambda x: "Miscellaneous" if pd.isna(x) or str(x).strip() == "" else x)
+df["Vendor"] = df["Vendor"].apply(lambda x: str(x).strip() if pd.notna(x) else x)
+
 
 
 # grabbing the amounts and round total (by 2 cents)
@@ -26,5 +29,5 @@ roundedTotal = round(total, 2)
 # Category totals
 categoryTotals = df.dropna(subset=["Category", "Amount"]).groupby("Category")["Amount"].sum().round(2)
 
-
+# graphics for the category (pie chart and totals) WIP
 
